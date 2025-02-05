6014b42d9c953487cfb3f04437acefbe54753c76
Jeffrey Varner <jdv27@cornell.edu>
2025-02-05 14:48:39 -0500

    L4a
    L4a

--- lectures/week-4/L4a/src/Compute.jl
+++ lectures/week-4/L4a/src/Compute.jl
@@ -47,15 +47,50 @@ function binary(S::Array{Float64,2})::Array{Int64,2}
     return B;
 end
 
-function reaction(metabolites::Dict{String,Any})::String
+function reactionstring(metabolites::Dict{String,Any})::String
 
     # initialize -
     reactant_string = "";
     product_string = "";
     arrow = "=";
 
+    # we need to iterate over the keys and values of the dictionary, and then we can concatenate the strings
+    species_list = keys(metabolites) |> collect |> sort; # sort the species, alphabetically
+    reactants = Dict{String,Any}();
+    products = Dict{String,Any}();
+    for species ∈ species_list
+        if metabolites[species] < 0
+            reactants[species] = metabolites[species];
+        elseif metabolites[species] > 0
+            products[species] = metabolites[species];
+        end
+    end
 
+    # iterate over the reactants
+    for (species, stoichiometry) ∈ reactants
+        reactant_string *= "$(abs(stoichiometry)) " * species * " + ";
+    end
+    reactant_string = reactant_string[1:end-3]; # remove the last " + "
+
+    # iterate over the products
+    for (species, stoichiometry) ∈ products
+        product_string *= "$(abs(stoichiometry)) " * species * " + ";
+    end
+    product_string = product_string[1:end-3]; # remove the last " + "
 
     # return -
-    return reactant_string * " $(arrow) " * product_string;
+    return reactant_string * " = " * product_string;
+end
+
+
+function softmax(x::Array{Float64,1})::Array{Float64,1}
+    
+    # compute the exponential of the vector
+    y = exp.(x);
+    
+    # compute the sum of the exponential
+    s = sum(y);
+    
+    # compute the softmax
+    return y / s;
 end
