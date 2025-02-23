0aa0aedcf1a762f57506c083a55c914d8c4c5eee
Jeffrey Varner <jdv27@cornell.edu>
2025-02-23 17:47:44 -0500

    update
    update

--- lectures/week-6/L6a/src/Parser.jl
+++ lectures/week-6/L6a/src/Parser.jl
@@ -13,7 +13,7 @@ function read_reaction_file(path_to_file::String)::Array{String,1}
     # Read in the file -
     open("$(path_to_file)", "r") do file
         for line in eachline(file)
-            +(vff_file_buffer,line)
+            +(vff_file_buffer, line)
         end
     end
 
